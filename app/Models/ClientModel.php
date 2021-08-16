0ed2949eab9194326c812a280b55412c85e4d9fc
Edwin Benalcázar <softwareywebsoluciones@gmail.com>
2021-08-16 09:58:59 -0500

    ADD: ClientModel

---
+++ app/Models/ClientModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ClientModel extends Model 
+{
+    protected $table = 'client';
+    protected $allowedFields = [
+        'name', 'email', 'retainer_fee'
+    ];
+
+    protected $useTimestamps = true;
+    protected $updatedField = 'updated_at';
+
+    public function findClientById($id)
+    {
+        $client = $this->asArray()->where(['id' => $id])->first();
+
+        if (!$client) 
+        {
+            throw new \Exception('Could not find client for specified ID');
+        }
+
+        return $client;
+    }
+}
